b8f7a46ad24ad68936e197dbcd53343dcd255712
Errortoast <141260128+Errortoast@users.noreply.github.com>
2025-04-09 10:32:52 -0700

    Update main.py
    Removed broken code tl retry l8r

--- main.py
+++ main.py
@@ -11,31 +11,21 @@ option1Text = "1";
 option2Text = "2";
 option3Text = "3";
 
-currentLine=1;
-
-option1line = 0;
-option2line = 0;
-option3line = 0;
+currentLine = 1;
 
 def option1Pressed():
     global currentLine, option1line;
     print("option 1 pressed");
-    currentLine = option1line;
-    print(str(currentLine) + ":" + str(option1line));
     nextOption();
 
 def option2Pressed():
     global currentLine, option2line;
     print("option 2 pressed");
-    currentLine = option2line;
-    print(str(currentLine) + ":" + str(option2line));
     nextOption();
 
 def option3Pressed():
-    global currentLine, option3line;
+    global currentLine;
     print("option 3 pressed");
-    currentLine = option3line;
-    print(str(currentLine) + ":" + str(option3line));
     nextOption();
 
 option1 = tk.Button(text=option1Text, command=option1Pressed);
@@ -57,21 +47,18 @@ def updateButtons(numButtons):
     if (numButtons>2): option3.pack(padx=20, pady=20);
 
 def nextOption():
-    global currentLine, option1Text, option2Text, option3Text, option1line, option2line, option3line;
+    global currentLine, option1Text, option2Text, option3Text;
     numTabs = (len(story[currentLine].split(":")[0]) / 2);
     question = story[currentLine-1].split(":")[2];
     numOptionsUsed = 0;
-    for i, line in enumerate(story[currentLine:], start=currentLine):
+    for line in story[currentLine:]:
         if len(line.split(":")[0]) / 2 == numTabs:
             if numOptionsUsed < 1:
                 option1Text = line.split(":")[1];
-                option1line = i;
             elif numOptionsUsed < 2:
                 option2Text = line.split(":")[1];
-                option2line = i;
             elif numOptionsUsed < 3:
                 option3Text = line.split(":")[1];
-                option3line = i;
             numOptionsUsed += 1;
         elif len(line.split(":")[0]) / 2 < numTabs:
             break;
