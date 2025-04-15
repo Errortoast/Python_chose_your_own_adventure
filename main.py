1db96aeb911710f83fad2bf6653e9779d55bd0cb
Errortoast <141260128+Errortoast@users.noreply.github.com>
2025-04-15 10:23:40 -0700

    Fixed exec functionality

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
-from ast import Try
-from asyncio.windows_events import NULL
 import tkinter as tk;
+import time;
 
 storyFile = open("story.txt", "r");
 story = [line.rstrip('\n') for line in storyFile.readlines()];
 storyFile.close();
+
+# Read the variables file
+with open("variables.txt", "r") as var_file:
+    variables_code = var_file.read()
+exec(variables_code, globals())
 
 window = tk.Tk();
 window.title("Hello World");
@@ -12,12 +16,17 @@ window.title("Hello World");
 option1Text = "1";
 option2Text = "2";
 option3Text = "3";
+option4Text = "4";
+option5Text = "5";
 
 currentLine = 0;
+oldLine = 0;
 
 option1line = 0;
 option2line = 0;
 option3line = 0;
+option4line = 0;
+option5line = 0;
 
 question ="Lorem Ipsum";
 
@@ -39,54 +48,98 @@ def option3Pressed():
     currentLine = option3line;
     nextOption();
 
+def option4Pressed():
+    global currentLine, option4line;
+    print("option 4 pressed");
+    currentLine = option4line;
+    nextOption();
+
+def option5Pressed():
+    global currentLine, option5line;
+    print("option 5 pressed");
+    currentLine = option5line;
+    nextOption();
+
 option1 = tk.Button(text=option1Text, command=option1Pressed);
 option2 = tk.Button(text=option2Text, command=option2Pressed);
 option3 = tk.Button(text=option3Text, command=option3Pressed);
+option4 = tk.Button(text=option4Text, command=option4Pressed);
+option5 = tk.Button(text=option5Text, command=option5Pressed);
 
 text = tk.Text(window, height=5, width=50);
 
 def updateButtons(numButtons):
-    global option1, option2, option3, text;
+    global option1, option2, option3, option4, option5, text;
     option1.destroy();
     option2.destroy();
     option3.destroy();
+    option4.destroy();
+    option5.destroy();
     text.delete("1.0", tk.END);
 
     option1 = tk.Button(text=option1Text, command=option1Pressed);
     option2 = tk.Button(text=option2Text, command=option2Pressed);
     option3 = tk.Button(text=option3Text, command=option3Pressed);
+    option4 = tk.Button(text=option4Text, command=option4Pressed);
+    option5 = tk.Button(text=option5Text, command=option5Pressed);
     text.insert('1.0', question);
 
     text.pack(padx=20, pady=20);
     if (numButtons>0): option1.pack(padx=20, pady=20);
     if (numButtons>1): option2.pack(padx=20, pady=20);
     if (numButtons>2): option3.pack(padx=20, pady=20);
+    if (numButtons>3): option4.pack(padx=20, pady=20);
+    if (numButtons>4): option5.pack(padx=20, pady=20);
 
 def nextOption():
-    global currentLine, option1Text, option2Text, option3Text, option1line, option2line, option3line, question;
+    global currentLine, option1Text, option2Text, option3Text, option4Text, option5Text, option1line, option2line, option3line, option4line, option5line, question;
+    try:
+        code_str = story[currentLine].split(":")[3]
+        code_str = bytes(code_str, "utf-8").decode("unicode_escape")
+        exec(code_str, globals())
+    except IndexError:
+        print("Expected code segment not found in the story line.");
+    except Exception as e:
+        print(f"Error executing code: {e}");
     numTabs = (len(story[currentLine].split(":")[0]));
     question = story[currentLine].split(":")[2];
+    option2Enabled = True;
+    option3Enabled = True;
+    option4Enabled = True;
+    option5Enabled = True;
     numOptionsUsed = 0;
+    numOptionsFound = 0;
     for i, line in enumerate(story[currentLine+1:]):
         if len(line.split(":")[0]) == numTabs+1:
-            if numOptionsUsed < 1:
+            if numOptionsFound < 1:
                 option1Text = line.split(":")[1];
                 option1line = currentLine+1+i;
-            elif numOptionsUsed < 2:
+                numOptionsUsed += 1;
+            elif numOptionsFound < 2 and option2Enabled == True:
                 option2Text = line.split(":")[1];
                 option2line = currentLine+1+i;
-            elif numOptionsUsed < 3:
+                numOptionsUsed += 1;
+            elif numOptionsFound < 3 and option3Enabled == True:
                 option3Text = line.split(":")[1];
                 option3line = currentLine+1+i;
-            numOptionsUsed += 1;
-            try:
-                line.split(":")[3];
-                print("Execute code");
-            except:
-                print("No code found");
+                numOptionsUsed += 1;
+            elif numOptionsFound < 4 and option4Enabled == True:
+                option4Text = line.split(":")[1];
+                option4line = currentLine+1+i;
+                numOptionsUsed += 1;
+            elif numOptionsFound < 5 and option5Enabled == True:
+                option5Text = line.split(":")[1];
+                option5line = currentLine+1+i;
+                numOptionsUsed += 1;
+            numOptionsFound +=1;
         elif len(line.split(":")[0]) <= numTabs:
             break;
     updateButtons(numOptionsUsed);
+
+def restart():
+    global currentLine;
+    currentLine=0;
+    nextOption();
 
 
 #region MAIN
@@ -94,4 +147,4 @@ nextOption();
 #endregion
 
 # Start the event loop.
-window.mainloop()
+window.mainloop();
